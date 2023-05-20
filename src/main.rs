f596075142a51791293a9c92546c18276fd236fd
krishna <saikrishna1908@gmail.com>
2023-05-20 19:03:32 +0530

    read input from user

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,42 @@
 
 mod model;
 
-use std::error::Error;
-
-extern  crate reqwest;
+use std::{error::Error, io::{self, BufRead}};
 use crate::model::*;
+
 
 
 #[tokio::main]
 async fn main()  -> Result<(), Box<dyn Error>>{    
-    let word = String::from("hello");
-    match reqwest::get(format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", word)).await {
-        Ok(response) => {
-            if response.status() == reqwest::StatusCode::OK {
-                match response.json::<Vec<Word>>().await {
-                    Ok(res) => {
-                        for i in 0..res.len() {
-                            println!("{}",res[i].to_string())
-                        }
-                    },
-                    Err(_) => println!("Failed to parse respnose to json")
-                }                
+    let mut lines = io::stdin().lock().lines();
+    
+
+    // let stdin = io::stdin();
+
+    while let Some(line) = lines.next() {
+        let last_input = line.unwrap();
+
+        if last_input == "exit" {
+            break;
+        } else {
+            match reqwest::get(format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", last_input)).await {
+                Ok(response) => {
+                    if response.status() == reqwest::StatusCode::OK {
+                        match response.json::<Vec<Word>>().await {
+                            Ok(res) => {
+                                for i in 0..res.len() {
+                                    println!("{}",res[i].to_string())
+                                }
+                            },
+                            Err(_) => println!("Failed to parse respnose to json")
+                        }                
+                    }
+                },
+                Err(_) => println!("Could not make the request")
             }
-        },
-        Err(_) => println!("Could not make the request")
+        }
     }
+    
+    
     Ok(())
 }
